e97abae91211a7fb0efa1ab176515fc2cd63414a
Siliphen <scorpion2007@qq.com>
2024-11-03 21:10:52 +0800

    1

---
+++ assets/resources/index.js
@@ -0,0 +1,22 @@
+System.register("chunks:///_virtual/resources",["./zh.ts"],(function(){return{setters:[null],execute:function(){}}}));
+
+System.register("chunks:///_virtual/zh.ts",["cc"],(function(e){var r;return{setters:[function(e){r=e.cclegacy}],execute:function(){r._RF.push({},"3e4f3AB+wdFfLSGEqGlo9Ic","zh",void 0);var c=window,n=e("languages",{test:{main:"测试",hello:"你好"},BuffDescrLookFar:"视野范围增加50%",BuffDescrScoreAdd:"获得分数提高200%",BuffDescrShield:"保护角色蛇身体节点不会被撞到",BuffDescrAttract:"食物的吸收范围扩大50%",ArticleName_LookFar:"放大镜",ArticleName_ScoreAdd:"加分药水",ArticleName_Shield:"护盾",ArticleName_Attract:"吸铁石",ArticleName_ResurrectionCard:"复活卡",AdCantPlay:"抱歉，遇到错误，无法播放广告。\n我们正在紧急修复。",UiMoney_onClick_Msg:"金币获取方式：\n通过在游戏中收集金币。",MoneyNotEnough:"金币不足。\n\n金币获取方式：通过在游戏中收集金币。",MoneyNotEnough2:"金币不足。",PropEmpty:"道具用完了，可在局外商店用金币购买。",ResurrectionCardNotEnough:"复活卡不足",TaskDescrSingle:"单局内",TaskDescrTotal:"共计",TaskDescrKill:"击杀{n}个竞争者",TaskDescrScore:"得分超过{n}分",TaskDescrFood:"吃下{n}个食物",TaskDescrMoney:"收集{n}个金币",TaskDescrLive:"生存{n}分钟"});c.languages||(c.languages={}),c.languages.zh=n,r._RF.pop()}}}));
+
+(function(r) {
+  r('virtual:///prerequisite-imports/resources', 'chunks:///_virtual/resources'); 
+})(function(mid, cid) {
+    System.register(mid, [cid], function (_export, _context) {
+    return {
+        setters: [function(_m) {
+            var _exportObj = {};
+
+            for (var _key in _m) {
+              if (_key !== "default" && _key !== "__esModule") _exportObj[_key] = _m[_key];
+            }
+      
+            _export(_exportObj);
+        }],
+        execute: function () { }
+    };
+    });
+});
